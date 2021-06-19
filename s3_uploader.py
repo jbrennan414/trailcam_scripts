222a152996767a741eff797db78f80aa503be7e2
John Brennan <brennanj414@gmail.com>
2021-06-19 12:35:00 -0600

    improvements for multiple files s3 uploader

--- s3_uploader.py
+++ s3_uploader.py
@@ -1,9 +1,26 @@
 import boto3
+import os
+import shutil
 
 s3 = boto3.resource("s3")
 
-#for bucket in s3.buckets.all():
-#  print(bucket.name)
+#list all files
+arr = os.listdir('/home/pi/trailcam_scripts/photos')
 
-data = open('/home/pi/trailcam_scripts/photos/image.jpg', 'rb')
-s3.Bucket('trailcam-photos').put_object(Key='image.jpg', Body=data)
+#upload each file
+for file in arr:
+  data = open(f'/home/pi/trailcam_scripts/photos/{file}', 'rb')
+  s3.Bucket('trailcam-photos').put_object(Key=file, Body=data)
+
+
+#todo this should be a try/catch...
+#delete photos directory
+dir_path = '/home/pi/trailcam_scripts/photos1'
+
+try:
+  shutil.rmtree(dir_path)
+except OSError as e:
+  print("Ok that didnt work")
+
+#make a new photos directory
+os.mkdir(dir_path)
--- trailcamera.py
+++ trailcamera.py
@@ -3,7 +3,7 @@ from picamera import PiCamera
 from datetime import datetime
 from gpiozero import LED
 from time import sleep
-
+#import os
 
 
 pir = MotionSensor(4)
@@ -12,14 +12,17 @@ now = datetime.now()
 
 date = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
 led = LED(17)
+#os.chdir(r"/home/pi/trailcam_scripts/photos")
 
-while True: 
-  pir.wait_for_motion()
-  print("Motion detected")
-  led.on()
-  camera.start_preview()
-  camera.capture('/home/pi/trailcam_scripts/photos/image.jpg')
-  camera.stop_preview()
-  pir.wait_for_no_motion()
-  print("Motion stopped")
-  led.off()
+camera.capture(date + ".jpg")
+
+#while True: 
+ # pir.wait_for_motion()
+ # print("Motion detected")
+ # led.on()
+ # camera.start_preview()
+ # camera.capture('/home/pi/trailcam_scripts/photos/image.jpg')
+ # camera.stop_preview()
+ # pir.wait_for_no_motion()
+ # print("Motion stopped")
+ # led.off()
