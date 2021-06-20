f6846ff801a142b4bcc3c29ce14dde71b6cbd562
John Brennan <brennanj414@gmail.com>
2021-06-20 15:20:18 -0600

    fix file overwrite issue

--- trailcamera.py
+++ trailcamera.py
@@ -8,15 +8,16 @@ pir = MotionSensor(4)
 camera = PiCamera()
 now = datetime.now()
 
-date = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
 led = LED(17)
 
 while True: 
   pir.wait_for_motion()
   print("Motion detected")
+  date = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
   led.on()
   camera.start_preview()
   camera.capture("/home/pi/trailcam_scripts/photos/{}.jpg".format(date))
+  print("/home/pi/trailcam_scripts/photos/{}.jpg".format(date))
   camera.stop_preview()
   pir.wait_for_no_motion()
   print("Motion stopped")
